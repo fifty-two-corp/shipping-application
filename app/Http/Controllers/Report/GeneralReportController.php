e969897edcb6bdc7b25baf3a807e9ac8dae02432
Bayu Trisnapati <bayutrisnapati@gmail.com>
2018-02-15 20:21:10 +0800

    add general report

--- app/Http/Controllers/Report/GeneralReportController.php
+++ app/Http/Controllers/Report/GeneralReportController.php
@@ -8,26 +8,56 @@ use App\Shipping;
 use App\Termin;
 use Auth;
 use PDF;
-use Validator;
 use Response;
 use Carbon\Carbon;
 
 class GeneralReportController extends Controller {
-    
+
     public function index() {
       return view('report/general-report/index');
     }
 
     public function ReportData (Request $request) {
       $date_start     = Carbon::parse($request->date_start)->format('Y-m-d');
-      $date_end       = Carbon::parse($request->date_end)->format('Y-m-d');
-      $termin_data    = Termin::whereBetween('payment_date',[$date_start, $date_end])->get();
-      $income_termin  = $termin_data->sum('payment');
-      $payoff_data    = Shipping::whereBetween('created_at',[$date_start, $date_end])->whereNull('time_period')->get();
-      $income_payoff  = $payoff_data->sum('cost');
-      $income_total   = $income_termin + $income_payoff;
-      
-      return view('report/general-report/result', compact('termin_data'));
+      $date_end       = Carbon::parse($request->date_end)->addDays(1)->format('Y-m-d');
+//      $date_start = '2017-10-01';
+//      $date_end   = '2018-03-01';
+      $date_range = Carbon::parse($request->date_start)->format('d/m/Y')." - ".Carbon::parse($request->date_end)->format('d/m/Y');
+
+      $transaction  = Shipping::with(['termin' => function ($query) use ($date_start, $date_end){
+        $query->whereBetween('payment_date',[$date_start, $date_end]);
+      }])->with('shipping_customer', 'shipping_destination')->whereBetween('created_at',[$date_start, $date_end])->get();
+
+      $payoff_total = $this->get_pay_off_total($date_start, $date_end);
+
+      $termin_total = $transaction->sum(function ($transaction) {
+        return $transaction->termin->sum('payment');
+      });
+      $income_total = $payoff_total + $termin_total;
+      $tax_total = $transaction->sum('tax_cost');
+      $opcost_total = $transaction->sum('operational_cost');
+
+      //return Response::json($transaction);
+      return view('report/general-report/result', compact('transaction','income_total', 'tax_total', 'opcost_total', 'date_range'));
+
+    }
+
+    public function get_pay_off_total($date_start, $date_end) {
+
+      $transaction  = Shipping::with(['termin' => function ($query) use ($date_start, $date_end){
+        $query->whereBetween('payment_date',[$date_start, $date_end]);
+      }])->whereBetween('created_at',[$date_start, $date_end])->get();
+
+      $filtered = $transaction->filter(function ($transaction, $key) {
+        return $transaction->payment_type == 'pay_off';
+      });
+
+      $vendor_total = $filtered->sum('vendor_cost');
+      $default_total = $filtered->sum('default_cost');
+      $payoff_total = $vendor_total + $default_total;
+
+      return $payoff_total;
+
     }
 
 }
--- resources/views/report/general-report/result.blade.php
+++ resources/views/report/general-report/result.blade.php
@@ -1,5 +1,76 @@
-<section style='position: relative;clear: both;margin: 5px 0;height: 1px;border-top: 1px solid #cbcbcb;margin-bottom: 25px;margin-top: 10px;text-align: center;'>
-	<h3 align='center' style='margin-top: -12px;background-color: #FFF;clear: both;width: 180px;margin-right: auto;margin-left: auto;padding-left: 15px;padding-right: 15px; font-family: arial,sans-serif;'>
-		<span>REPORT</span>
-	</h3>
-</section>
+{{--<section style='position: relative;clear: both;margin: 5px 0;height: 1px;border-top: 1px solid #cbcbcb;margin-bottom: 25px;margin-top: 10px;text-align: center;'>--}}
+	{{--<h3 align='center' style='margin-top: -12px;background-color: #FFF;clear: both;width: 180px;margin-right: auto;margin-left: auto;padding-left: 15px;padding-right: 15px; font-family: arial,sans-serif;'>--}}
+		{{--<span>REPORT</span>--}}
+	{{--</h3>--}}
+{{--</section>--}}
+<div class="container">
+	<h2>General Report</h2>
+	<button style="float: right" class="btn btn-info">Save PDF</button><br>
+	<p>Date : {{$date_range}}</p>
+	<table class="table table-bordered">
+		<thead style="background-color: whitesmoke">
+		<tr>
+			<th>Shipping</th>
+			<th>Date</th>
+			<th width="18%">Income</th>
+			<th width="18%">Tax</th>
+			<th width="18%">Operational Cost</th>
+		</tr>
+		</thead>
+		<tbody>
+		@foreach($transaction as $transactions)
+		<tr>
+			<td>
+				<strong><u>#{{$transactions->transaction_number}}</u></strong><br>
+				{{$transactions->shipping_customer->customer_name}}<br>
+				Shipping Method : <strong>{{$transactions->shipping_method}}</strong><br>
+				Dest : {{$transactions->shipping_destination['consignee_city']}} - {{$transactions->shipping_destination['consignee_province']}}<br>
+				@if($transactions->shipping_method == 'default')
+					Shipping Cost : <strong><span style="float: right">{{number_format($transactions->default_cost,0,',','.')}}</span></strong>
+				@elseif($transactions->shipping_method == 'vendor')
+					Shipping Cost : <strong><span style="float: right">{{number_format($transactions->vendor_cost,0,',','.')}}</span></strong>
+				@endif
+			</td>
+			<td>
+				<b>{{date('d-m-Y',strtotime($transactions->created_at))}}</b><br>
+				@if($transactions->payment_type == 'installment')
+					@foreach($transactions->termin as $termin)
+						{{date('d-m-Y',strtotime($termin->payment_date))}}<br>
+					@endforeach
+				@endif
+			</td>
+			<td>
+				@if($transactions->payment_type == 'pay_off')
+					@if($transactions->shipping_method == 'default')
+						<strong>Pay Off </strong><br>
+						<span style="float: right">{{number_format($transactions->default_cost,0,',','.')}}</span>
+					@elseif($transactions->shipping_method == 'vendor')
+						<strong>Pay Off </strong><br>
+						<span style="float: right">{{number_format($transactions->vendor_cost,0,',','.')}}</span>
+					@endif
+				@elseif($transactions->payment_type == 'installment')
+					<strong>Termin ( {{$transactions->time_period}} days )</strong><br>
+					@foreach($transactions->termin as $termin)
+						<span style="float: right">{{number_format($termin->payment,0,',','.')}}</span><br>
+					@endforeach
+				@endif
+			</td>
+			<td>
+				<strong>{{$transactions->tax_value}}%</strong><br>
+				<span style="float: right">{{number_format($transactions->tax_cost,0,',','.')}}</span>
+			</td>
+			<td><span style="float: right">{{number_format($transactions->operational_cost,0,',','.')}}</span></td>
+		</tr>
+		@endforeach
+		</tbody>
+		<tfoot>
+		<tr>
+			<td colspan="2"><strong>TOTAL</strong></td>
+			<td><span style="float: right"><strong>{{number_format($income_total,0,',','.')}}</strong></span></td>
+			<td><span style="float: right"><strong>{{number_format($tax_total,0,',','.')}}</strong></span></td>
+			<td><span style="float: right"><strong>{{number_format($opcost_total,0,',','.')}}</strong></span></td>
+		</tr>
+		</tfoot>
+	</table>
+</div>
+<hr>
