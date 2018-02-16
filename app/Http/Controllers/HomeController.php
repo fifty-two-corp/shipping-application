ebd30cab35cc5f3fa2f18126336b69c5b9c46515
Bayu Trisnapati <bayutrisnapati@gmail.com>
2018-02-16 23:31:51 +0800

    update dashboard grafik

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Shipping;
+use App\Termin;
+use Response;
 //use Chartjs;
 
 class HomeController extends Controller
@@ -25,8 +27,33 @@ class HomeController extends Controller
     public function index(Request $request) {
         $shipping_pending = Shipping::where('status', 'Pending')->count();
         $total_income = "Rp. ".number_format(Shipping::sum('cost'),0,',','.');
+
+        $months = [1,2,3,4,5,6,7,8,9,10,11,12];
+        $year = date('Y');
+
+        foreach ($months as $month) {
+          $taxs = Shipping::whereYear('created_at',$year)->whereMonth('created_at', $month)->sum('tax_cost');
+          $tax[] = $taxs;
+
+          $opcost = Shipping::whereYear('created_at',$year)->whereMonth('created_at', $month)->sum('operational_cost');
+          $opscost[] = $opcost;
+
+          $earnings_vendor = Shipping::where('shipping_method','=', 'vendor')
+              ->where('payment_type','=','pay_off')
+              ->whereYear('created_at',$year)
+              ->whereMonth('created_at', $month)
+              ->sum('vendor_cost');
+          $earnings_default = Shipping::where('shipping_method','=', 'default')
+              ->where('payment_type','=','pay_off')
+              ->whereYear('created_at',$year)
+              ->whereMonth('created_at', $month)
+              ->sum('default_cost');
+          $termin = Termin::whereYear('payment_date',$year)->whereMonth('payment_date', $month)->sum('payment');
+          $income[] = $earnings_vendor + $earnings_default + $termin;
+        }
+
         $chartjs = app()->chartjs
-        ->name('lineChartTest')
+        ->name('jujurperkasaChart')
         ->type('line')
         ->size(['width' => 400, 'height' => 200])
         ->labels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'])
@@ -39,20 +66,49 @@ class HomeController extends Controller
                 "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
                 "pointHoverBackgroundColor" => "rgba(13, 6, 11, 0.58)",
                 "pointHoverBorderColor" => "rgba(13, 6, 11, 0.58)",
-                'data' => [65, 59, 80, 81, 56, 55, 40],
+                'data' => $income,
             ],
             [
-                "label" => "My Second dataset",
+                "label" => "Ops. Cost",
                 'backgroundColor' => "rgba(0, 0, 0, 0)",
                 'borderColor' => "rgba(188, 40, 79, 0.87)",
                 "pointBorderColor" => "rgba(188, 40, 79, 0.87)",
                 "pointBackgroundColor" => "rgba(188, 40, 79, 0.87)",
                 "pointHoverBackgroundColor" => "rgba(13, 6, 11, 0.58)",
                 "pointHoverBorderColor" => "rgba(13, 6, 11, 0.58)",
-                'data' => [12, 33, 44, 44, 55, 23, 40],
+                'data' => $opscost,
+            ],
+            [
+                "label" => "Tax",
+                'backgroundColor' => "rgba(0, 0, 0, 0)",
+                'borderColor' => "rgba(66, 108, 217, 1)",
+                "pointBorderColor" => "rgba(66, 108, 217, 1)",
+                "pointBackgroundColor" => "rgba(66, 108, 217, 1)",
+                "pointHoverBackgroundColor" => "rgba(66, 108, 217, 1)",
+                "pointHoverBorderColor" => "rgba(13, 6, 11, 0.58)",
+                'data' => $tax,
             ]
         ])
-        ->options([]);
+            ->optionsRaw("{
+              tooltips: {
+                callbacks: {
+                  label: function (tooltipItem, data) {
+                    return data.datasets[tooltipItem.datasetIndex].label + ' - ' +  Number(tooltipItem.yLabel).toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, \",\");
+                  },
+                },
+              },
+              scales: {
+                  yAxes: [{
+                    ticks: {
+                      beginAtZero:true,
+                      callback: function(value, index, values) {
+                        return Number(value).toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, \",\");
+                      }
+                    }
+                  }]
+                }
+            }");
         return view('home', compact('shipping_pending', 'total_income','chartjs'));
+        //return Response::json($test);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,11 @@
   <h1 class="page-header">Dashboard <small></small></h1>
   <div class="row">
     <!-- begin col-3 -->
-    <div class="col-md-3 col-sm-6">
+    <div class="col-md-2 col-sm-6">
       <div class="widget widget-stats bg-green">
         <div class="stats-icon"><i class="fa fa-money"></i></div>
         <div class="stats-info">
-          <h4>TOTAL INCOME</h4>
+          <h4>EARNING AFTER TAX (2018)</h4>
           <p>{{ $total_income }}</p>    
         </div>
         <div class="stats-link">
@@ -18,11 +18,11 @@
     </div>
     <!-- end col-3 -->
     <!-- begin col-3 -->
-    <div class="col-md-3 col-sm-6">
-      <div class="widget widget-stats bg-blue">
+    <div class="col-md-2 col-sm-6">
+      <div class="widget widget-stats bg-orange">
         <div class="stats-icon"><i class="fa fa-chain-broken"></i></div>
         <div class="stats-info">
-          <h4>BOUNCE RATE</h4>
+          <h4>INSTALLMENT TERM DEBT</h4>
           <p>20.44%</p>   
         </div>
         <div class="stats-link">
@@ -32,11 +32,11 @@
     </div>
     <!-- end col-3 -->
     <!-- begin col-3 -->
-    <div class="col-md-3 col-sm-6">
-      <div class="widget widget-stats bg-purple">
+    <div class="col-md-2 col-sm-6">
+      <div class="widget widget-stats bg-red">
         <div class="stats-icon"><i class="fa fa-users"></i></div>
         <div class="stats-info">
-          <h4>UNIQUE VISITORS</h4>
+          <h4>DUE DATE TERMIN</h4>
           <p>1,291,922</p>    
         </div>
         <div class="stats-link">
@@ -46,8 +46,22 @@
     </div>
     <!-- end col-3 -->
     <!-- begin col-3 -->
-    <div class="col-md-3 col-sm-6">
-      <div class="widget widget-stats bg-red">
+    <div class="col-md-2 col-sm-6">
+      <div class="widget widget-stats bg-blue">
+        <div class="stats-icon"><i class="fa fa-clock-o"></i></div>
+        <div class="stats-info">
+          <h4>SHIPPING PENDING</h4>
+          <p>{{ $shipping_pending }}</p>
+        </div>
+        <div class="stats-link">
+          <a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+        </div>
+      </div>
+    </div>
+    <!-- end col-3 -->
+    <!-- begin col-3 -->
+    <div class="col-md-2 col-sm-6">
+      <div class="widget widget-stats bg-blue">
         <div class="stats-icon"><i class="fa fa-clock-o"></i></div>
         <div class="stats-info">
           <h4>SHIPPING PENDING</h4>
